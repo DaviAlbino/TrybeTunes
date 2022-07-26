3998addb3d17bfd957ce7fd2b6c250235d26dbc1
Davi Albino <negroalbino_@hotmail.com>
2022-07-26 19:07:42 -0300

    Requisito 13

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import Header from './Header';
+import MusicCard from './MusicCard';
+// import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+// import Loading from './Loading';
 
 class Favorites extends React.Component {
+  state = {
+    favoritesSongsList: [],
+  }
+
+  componentDidMount = () => {
+
+  }
+
+  // getFavoriteList = async () => {
+  //   const myFavorites = await getFavoriteSongs();
+  // }
+
   render() {
+    const { favoritesSongsList } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         <h1> Favorites </h1>
+        { favoritesSongsList.map((song, index) => (
+          <div key={ index }>
+            <MusicCard
+              trackName={ song.trackName }
+              previewUrl={ song.previewUrl }
+              music={ song }
+            />
+
+          </div>
+        )) }
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 import Header from './Header';
 
 class Profile extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-profile">
-        <Header />
+  state = {
+    loadingApi: false,
+    infos: [],
+  }
 
-        <h1> Profile </h1>
+  componentDidMount() {
+    this.getInfos();
+  }
+
+  getInfos = async () => {
+    this.setState({ loadingApi: true });
+    const newInfos = await getUser();
+    this.setState({
+      infos: newInfos,
+      loadingApi: false,
+    });
+  }
+
+  render() {
+    const { infos, loadingApi } = this.state;
+    const { name, email, image, description } = infos;
+    return (
+      <div>
+        <Header />
+        {loadingApi ? <Loading /> : (
+          <div data-testid="page-profile">
+            <div data-testid="card-profile">
+              <img src={ image } alt={ name } data-testid="profile-image" />
+              <Link to="/profile/edit">Editar perfil</Link>
+              <p>{ name }</p>
+              <p>{ email }</p>
+              <p>{ description }</p>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
